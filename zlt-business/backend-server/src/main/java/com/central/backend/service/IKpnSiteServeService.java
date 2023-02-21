6168d99b1a42f44c40001f9a4fc35034ec5033fe
yixiu <yixiu19861102@gmail.com>
2023-02-21 20:21:54 +0800

    APP升级管理与客服中心配置

---
+++ zlt-business/backend-server/src/main/java/com/central/backend/service/IKpnSiteServeService.java
@@ -0,0 +1,23 @@
+package com.central.backend.service;
+
+import com.central.common.model.KpnSiteServe;
+import com.central.common.model.PageResult;
+import com.central.common.service.ISuperService;
+
+import java.util.Map;
+
+/**
+ * 站点客服配置
+ *
+ * @author yixiu
+ * @date 2023-02-21 19:46:07
+ */
+public interface IKpnSiteServeService extends ISuperService<KpnSiteServe> {
+    /**
+     * 列表
+     * @param params
+     * @return
+     */
+    PageResult<KpnSiteServe> findList(Map<String, Object> params);
+}
+
---
+++ zlt-business/backend-server/src/main/java/com/central/backend/service/impl/KpnSiteServeServiceImpl.java
@@ -0,0 +1,37 @@
+package com.central.backend.service.impl;
+
+import com.central.backend.mapper.KpnSiteServeMapper;
+import com.central.backend.service.IKpnSiteServeService;
+import com.central.common.model.KpnSiteServe;
+import org.springframework.stereotype.Service;
+import com.central.common.model.PageResult;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.central.common.service.impl.SuperServiceImpl;
+
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.collections4.MapUtils;
+import lombok.extern.slf4j.Slf4j;
+
+
+/**
+ * 站点客服配置
+ *
+ * @author yixiu
+ * @date 2023-02-21 19:46:07
+ */
+@Slf4j
+@Service
+public class KpnSiteServeServiceImpl extends SuperServiceImpl<KpnSiteServeMapper, KpnSiteServe> implements IKpnSiteServeService {
+    /**
+     * 列表
+     * @param params
+     * @return
+     */
+    @Override
+    public PageResult<KpnSiteServe> findList(Map<String, Object> params){
+        Page<KpnSiteServe> page = new Page<>(MapUtils.getInteger(params, "page"), MapUtils.getInteger(params, "limit"));
+        List<KpnSiteServe> list  =  baseMapper.findList(page, params);
+        return PageResult.<KpnSiteServe>builder().data(list).count(page.getTotal()).build();
+    }
+}
