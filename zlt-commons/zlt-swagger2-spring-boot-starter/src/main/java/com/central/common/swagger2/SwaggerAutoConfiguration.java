fc16954f9757ab224f88a29b8e8559d2b9b95c5d
yixiu <yixiu19861102@gmail.com>
2023-03-22 12:38:37 +0800

    20230322swager增加请求头参数

--- zlt-commons/zlt-swagger2-spring-boot-starter/src/main/java/com/central/common/swagger2/SwaggerAutoConfiguration.java
+++ zlt-commons/zlt-swagger2-spring-boot-starter/src/main/java/com/central/common/swagger2/SwaggerAutoConfiguration.java
@@ -49,6 +49,7 @@ import java.util.stream.Collectors;
 @Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerAutoConfiguration implements BeanFactoryAware {
     private static final String AUTH_KEY = "Authorization";
+    private static final String LANGUAGE = "language";
 
     private BeanFactory beanFactory;
 
@@ -214,6 +215,8 @@ public class SwaggerAutoConfiguration implements BeanFactoryAware {
         List<ApiKey> apiKeys = new ArrayList<>(1);
         ApiKey apiKey = new ApiKey(AUTH_KEY, AUTH_KEY, "header");
         apiKeys.add(apiKey);
+        ApiKey apiKey1 = new ApiKey(LANGUAGE, LANGUAGE, "header");
+        apiKeys.add(apiKey1);
         return apiKeys;
     }
 
